5022d49f7b2e3bf90fec51086318d0c1a584a8d9
aqachun <aqachun@gmail.com>
2025-08-26 16:10:15 +0800

    Quartz sync: Aug 26, 2025, 4:10 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'aqachun/quartz-notes',
+        // from data-repo-id
+        repoId: 'R_kgDOPkWfaA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOPkWfaM4CumMh',
+        // from data-lang
+        lang: 'en'
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
